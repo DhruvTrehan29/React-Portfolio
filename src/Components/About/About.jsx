b9455b38b14ea1921b61ce2af39a32755a4a871f
DhruvTrehan29 <dhruvtrehan29@gmail.com>
2026-08-08 14:19:25 +0530

    Deployment fix 2

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,44 +1,53 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/pic.png'
-import {BiCodeAlt} from 'react-icons/bi'
-import {BiCodeCurly} from 'react-icons/bi'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { BiCodeAlt, BiCodeCurly } from 'react-icons/bi'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-AOS.init();
-const about = () => {
+AOS.init()
+
+const About = () => {
   return (
-    <section id='About' data-aos="fade-left" data-aos-duration="1000">
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
-      <div className="container container__about">
+    <section id="About">
+      <div className="container about__container">
         <div className="about__me">
-        <div className="about__me-image">
-            <img src={ME} alt="No Image Found" />
+          <div className="about__me-image">
+            <img src={ME} alt="Dhruv Trehan" />
           </div>
         </div>
 
         <div className="about__content">
-         <div className="about__cards">
-          <article className='about__card'>
-              <BiCodeAlt className="about__icons"/>
-            <h5>Projects</h5>
-            <small>3+ live projects</small>
-          </article>
+          <div className="about__cards">
+            <article className="about__card">
+              <BiCodeAlt className="about__icons" />
+              <h5>Projects</h5>
+              <small>3+ live projects</small>
+            </article>
 
-          <article className='about__card'>
-          <BiCodeCurly className="about__icons"/>
-            <h5>Hackathons</h5>
-            <small>Participated in 2 Hackathon</small>
-          </article>
-         </div>
-         <p>I am currently pursuing Btech in Information Technology(7th Semester) from Dr. Akhilesh Das Gupta Institute Of  technology and Management. I am currently seeking a platform where I could utilize my technical skills and knowledge for the growth and productivity of the organization and also gain relevant experience out of it </p>
-         <a href="#Contact" className='btn btn-primary'>Let's Talk</a>
+            <article className="about__card">
+              <BiCodeCurly className="about__icons" />
+              <h5>Hackathons</h5>
+              <small>Participated in 2 Hackathon</small>
+            </article>
+          </div>
+
+          <p>
+            I am currently pursuing Btech in Information Technology (7th
+            Semester) from Dr. Akhilesh Das Gupta Institute Of Technology and
+            Management. I am currently seeking a platform where I could
+            utilize my technical skills and knowledge for the growth and
+            productivity of the organization and also gain relevant experience
+            out of it.
+          </p>
+
+          <a href="#Contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
   )
 }
 
-export default about
+export default About;
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,60 +1,113 @@
 import React from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {BsLinkedin} from 'react-icons/bs'
-import { useRef } from 'react';
+import { MdOutlineEmail } from 'react-icons/md'
+import { BsLinkedin } from 'react-icons/bs'
+import { useRef } from 'react'
 import emailjs from 'emailjs-com'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-AOS.init();
+AOS.init()
+
 const Contact = () => {
-  const form = useRef();
+  const form = useRef()
+
   const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs.sendForm('service_wj1onri', 'template_1a0lrzw', form.current, '7tUW9F0F2JGSjYQos')
-    .then((result) => {
-      console.log(result.text);
-    }, (error) => {
-      console.log(error.text);
-    });
-    e.target.reset();
-  };
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_wj1onri',
+        'template_1a0lrzw',
+        form.current,
+        '7tUW9F0F2JGSjYQos'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+
+    e.target.reset()
+  }
+
   return (
-    <section id='Contact' data-aos="fade-top" data-aos-duration="1000">
-      <h5>Get in Touch</h5>
-      <h2>Contact</h2>
-      <div className="container container__contact">
+    <section id="Contact">
+      <div className="container contact__container">
+        <h5>Get in Touch</h5>
+        <h2>Contact</h2>
+
         <div className="contact__options">
-        <a href="mailto:dhruvtrehan29@gmail.com" target="_blank">
-          <article className='contact__option'>
-          <MdOutlineEmail className='contact__option-icon'/>
-          <h4>Email</h4>
-          <h5>dhruvtrehan29@gmail.com</h5>
-          <a href="mailto:dhruvtrehan29@gmail.com" target="_blank">Send a message</a>
+
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+
+            <h4>Email</h4>
+
+            <h5>dhruvtrehan29@gmail.com</h5>
+
+            <a
+              href="mailto:dhruvtrehan29@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
-          </a>
-          <a href="https://www.linkedin.com/in/dhruv-trehan-014218201/" target="_blank">
-          <article className='contact__option'>
-          <BsLinkedin className='contact__option-icon'/>
-          <h4>LinkedIn Message</h4>
-          <h5>Dhruv Trehan</h5>
-          <a href="https://www.linkedin.com/in/dhruv-trehan-014218201/" target="_blank">Send a message</a>
+
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+
+            <h4>LinkedIn Message</h4>
+
+            <h5>Dhruv Trehan</h5>
+
+            <a
+              href="https://www.linkedin.com/in/dhruv-trehan/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
-          </a>
+
         </div>
+
         {/* End of the social media message option */}
 
         {/* Start of form */}
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder='Your Full Name' required />
-          <input type="email" name="email" placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            required
+          />
+
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
   )
 }
 
-export default Contact
+export default Contact;
