4efc266adf2efee33fdcea2e9fd1462292f0aaff
DhruvTrehan29 <dhruvtrehan29@gmail.com>
2026-08-08 14:13:02 +0530

    Deployment fix 2

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,29 +1,59 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {SiGeeksforgeeks} from 'react-icons/si'
-import{SiLeetcode} from 'react-icons/si'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { SiGeeksforgeeks } from 'react-icons/si'
+import { SiLeetcode } from 'react-icons/si'
 import './footer.css'
+
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Dhruv Trehan</a>
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+      <a href="#Home" className="footer__logo">Dhruv Trehan</a>
+
+      <ul className="permalinks">
+        <li><a href="#Home">Home</a></li>
         <li><a href="#About">About</a></li>
         <li><a href="#Skills">Skills</a></li>
         <li><a href="#Projects">Projects</a></li>
         <li><a href="#App">Timeline</a></li>
         <li><a href="#Contact">Contact</a></li>
       </ul>
+
       <div className="footer__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://auth.geeksforgeeks.org/user/dhruvtrehan29/practice/" target="_blank"><SiGeeksforgeeks/></a>
-        <a href="https://leetcode.com/DhruvTrehan/" target="_blank"><SiLeetcode/></a>
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+
+        <a
+          href="https://auth.geeksforgeeks.org/user/dhruvtrehan29/practice/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiGeeksforgeeks />
+        </a>
+
+        <a
+          href="https://leetcode.com/DhruvTrehan/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiLeetcode />
+        </a>
       </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {SiGeeksforgeeks} from 'react-icons/si'
-import{SiLeetcode} from 'react-icons/si'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { SiGeeksforgeeks } from 'react-icons/si'
+import { SiLeetcode } from 'react-icons/si'
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank" className='socialEdit'><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank" className='socialEdit'><FaGithub/></a>
-        <a href="https://auth.geeksforgeeks.org/user/dhruvtrehan29/practice/" target="_blank" className='socialEdit'><SiGeeksforgeeks/></a>
-        <a href="https://leetcode.com/DhruvTrehan/" target="_blank" className='socialEdit'><SiLeetcode/></a>
+      <a
+        href="https://linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+        className='socialEdit'
+      >
+        <BsLinkedin />
+      </a>
+
+      <a
+        href="https://github.com"
+        target="_blank"
+        rel="noreferrer"
+        className='socialEdit'
+      >
+        <FaGithub />
+      </a>
+
+      <a
+        href="https://auth.geeksforgeeks.org/user/dhruvtrehan29/practice/"
+        target="_blank"
+        rel="noreferrer"
+        className='socialEdit'
+      >
+        <SiGeeksforgeeks />
+      </a>
+
+      <a
+        href="https://leetcode.com/DhruvTrehan/"
+        target="_blank"
+        rel="noreferrer"
+        className='socialEdit'
+      >
+        <SiLeetcode />
+      </a>
     </div>
   )
 }
 
-export default HeaderSocials
+export default HeaderSocials;
